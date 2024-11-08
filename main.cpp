60aa0a1c3802bd9cd888f943f68fa6e7caec30c9
Elia Albaba <ea152@uakron.edu>
2024-11-07 20:50:38 -0500

    Add main code with introduction to the program

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <array>
+#include <string_view>
+#include <thread>
+#include <chrono>
+#include <random>
+
+int randomInt(int min, int max) {
+    int range = max - min + 1;
+    return rand() % range + min;
+}
+
+bool isNumber(const std::string& input)
+{
+    try {
+        std::stoi(input);
+        return true;
+    } catch (std::invalid_argument&) {
+        std::cerr << "That is not a number" << std::endl;
+        return false;
+    } catch (std::out_of_range&) {
+        std::cerr << "Invalid input" << std::endl;
+        return false;
+    }
+    return false;
+}
+
+int main()
+{
+    // Welcome message
+    std::cout << "Welcome to Numeber Guesser 1.0" << std::endl;
+    std::cout << "This program tries to guess the number you have in mind" << std::endl;
+    std::cout << "Let's start, please pick a number between [-255, and 255]" << std::endl;
+
+    return 0;
+}
